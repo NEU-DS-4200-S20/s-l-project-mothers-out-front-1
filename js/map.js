eccc46475fdfff5006852c87a757b1c6594d35d1
Starcrab <Starcrab@nancydeMacBook-Air.local>
2020-04-04 16:58:02 -0400

    zoom-in function and parts of choropleth

--- js/map.js
+++ js/map.js
@@ -1,6 +1,7 @@
 // initializing constants for map
-var height = 800;
-var width = 1500;
+var height = 800,
+    width = 1500,
+    centered;
 
 var svg = d3
 .select("#map-holder")
@@ -16,14 +17,34 @@ var projection = d3
 var path = d3.geoPath().projection(projection);
 
 d3.json("files/us.json", function(us) {
-  d3.csv("data/by-state.csv", function(states) {
-    drawMap(us, states);
-  })
+  d3.csv("data/by-city.csv", function(cities) {
+    d3.csv('data/by-state.csv',function(states){
+      d3.tsv('data/us-state-names.tsv',function(stateNames){
+         drawMap(us, cities, states, stateNames);
+      });
+    });
+  });
 });
 
-function drawMap(us, states) {
 
-    var mapGroup = svg.append("g").attr("class", "mapGroup");
+var mapGroup = svg.append("g").attr("class", "mapGroup");
+
+function drawMap(us, cities, states, stateNames) {
+
+  // Setting up Choropleth function [in process]
+    // let fillFunction = function(d){
+    //   console.log(d)
+    // let stateName = stateNames.filter(function (n) { return n.id == d.id })[0].name
+    // let statesTargetNames = states.map(function (s) { return s.name } );
+    // let isTarget = statesTargetNames.includes(stateName);
+    // }
+    //
+    // if (isTarget) {
+    //   return 'blue';
+    // } else {
+    //   return '#aaa';
+    // }
+  // end of Choropleth section.
 
     mapGroup.append("g")
     .attr("id", "states")
@@ -31,7 +52,21 @@ function drawMap(us, states) {
     .data(topojson.feature(us, us.objects.states).features).enter()
     .append("path")
     .attr("class", "states")
-    .attr("d", path);
+    .attr("d", path)
+    .on('click', clicked);
+    // .attr('fill', fillFunction)
+
+    // .on('mouseover', function(d) {
+    //   // console.log("mouseover state", d)
+    //   // console.log(this)
+    //   let state = d3.select(this);
+    //   state.attr("fill", "red");
+    // })
+    // .on('mouseout', function(d) {
+    //   let state = d3.select(this);
+    //   state.attr("fill", fillFunction);
+    // });
+
 
     mapGroup.append("path")
     .datum(
@@ -42,9 +77,43 @@ function drawMap(us, states) {
     .attr("id", "state-borders")
     .attr("d", path);
 
+     //
+     //
+     // var paletteScale = d3.scale.linear()
+     //             .domain([d3.min, d3.max])
+     //             .range(["#EFEFFF","#02386F"]); // blue color
+
+
     var legend = svg
     .append("g")
     .attr("class", "legend")
     .attr("width", 140)
     .attr("height", 400);
+
+
+};
+
+function clicked(d) {
+  var x, y, k;
+
+  if (d && centered !== d) {
+    var centroid = path.centroid(d);
+    x = centroid[0];
+    y = centroid[1];
+    k = 4;
+    centered = d;
+  } else {
+    x = width / 2;
+    y = height / 2;
+    k = 1;
+    centered = null;
+  }
+
+  mapGroup.selectAll("path")
+      .classed("active", centered && function(d) { return d === centered; });
+
+  mapGroup.transition()
+      .duration(750)
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+      .style("stroke-width", 1.5 / k + "px");
 }
